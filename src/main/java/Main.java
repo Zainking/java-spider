2023058609809f0e292af2cdca1215fc48574492
Zain <wangziyi.2019@bytedance.com>
2019-03-26 14:06:50 +0800

    Revert "Init"
    This reverts commit 1f65f0a724b8b3b2d9380a532b9f8dccbc4e9c9d.

--- src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-import controller.Run;
-
-public class Main {
-    public static void main(String[] args) {
-        Run.start();
-    }
-}
--- src/main/java/controller/Run.java
+++
@@ -1,7 +0,0 @@
-package controller;
-
-public class Run {
-    public static void start() {
-        System.out.println("Hello, world!");
-    }
-}
